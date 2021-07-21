4cfef7288fac5bead2d5e8089ae0452fa20c7690
Cameron <cameroncronheimer@gmail.com>
2021-07-21 18:32:27 -0400

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,9 +8,13 @@ const Home = () => {
   return(
        <>
       <Text>Cameron Cronheimer</Text>
+
+      <VidCard> 
       <video width="320" autoPlay muted loop playsInline>
           <source src={Car} type="video/mp4"/>
       </video>
+      </VidCard>
+     
        </>
   ) 
 };
@@ -21,4 +25,13 @@ const Text = styled.div`
   font-family: "SF Pro Rounded SemiBold";
   font-size: 24px;
   font-weight: 100px;
+`;
+
+const VidCard = styled.div`
+  border-radius: 22px;
+overflow: hidden;
+-webkit-transform: translateZ(0);
+box-shadow: 0 19px 51px 0 rgba(0,0,0,0.16), 0 14px 19px 0 rgba(0,0,0,0.07);
+ width: 320px;
+
 `;
